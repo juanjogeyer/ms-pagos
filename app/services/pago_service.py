cf250b3422bf2c50b3e6c48fa9b74b1fbef1ee32
Juan Geyer <juanjogeyer@gmail.com>
2024-12-04 12:33:01 -0300

    Update

--- app/services/pago_service.py
+++ app/services/pago_service.py
@@ -10,7 +10,6 @@ class PagoService:
         result = repository.add(pago)
         if result:
             cache.set(f"pago_{result.id}", result, timeout=60)
-            print(f"Pago {result.id} añadido al caché")  # Mensaje de depuración
         else:
             print("Error al añadir el pago en la base de datos")  # Mensaje de depuración
         return result
@@ -19,9 +18,7 @@ class PagoService:
         pago = self.find(id)
         if pago:
             cache.delete(f"pago_{id}")
-            print(f"Pago {id} eliminado del caché")  # Mensaje de depuración
             repository.delete(pago)
-            print(f"Pago {id} eliminado de la base de datos")  # Mensaje de depuración
             return True
         else:
             print(f"Pago {id} no encontrado en la base de datos")  # Mensaje de depuración
